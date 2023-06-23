1dc51845d30e64a221a8cd94b42a84c94df8cc7a
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-23 20:38:47 +0300

    Profil sayfası view hazırlandı

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', include([
         path('', main.homepage, name='homepage'),
         path('login/', auth_views.LoginView.as_view(template_name='user/users/login.html'), name='login'),
-        path('logout/', users.logout_page, name='logout'),
+        path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+        path('profile/', users.profile, name='profile'),
     ])),
 ]
--- apps/core/views/users.py
+++ apps/core/views/users.py
@@ -5,25 +5,9 @@ from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 
 
-# AUTHENTICATION VIEWS
-def login_page(request):
-    if request.user.is_authenticated:
-        return redirect('homepage')
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-        user = authenticate(request, username=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('homepage')
-        else:
-            messages.error(request, _('Böyle bir kullanıcı bulunamadı'))
-    return render(request, 'user/users/login.html')
-
-
-@login_required()
-def logout_page(request):
-    logout(request)
-    return redirect('login')
-
 # PROFILE VIEWS
+def profile(request):
+    user = request.user
+    return render(request, 'user/users/profile.html', {
+        'user': user
+    })
