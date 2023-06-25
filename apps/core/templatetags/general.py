36dc3c2eec7b535668a92749b29e271377346715
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-25 18:40:02 +0300

    Template üzerinde iyileştirmeler yapıldı ve requirements dosyası oluşturuldu

--- apps/core/templatetags/general.py
+++ apps/core/templatetags/general.py
@@ -1,3 +1,22 @@
 from django import template
+from apps.core.models import Users, Videos
 
 register = template.Library()
+
+
+@register.simple_tag()
+def students_count():
+    students = Users.objects.filter(groups__name='Öğrenci')
+    return students.count()
+
+
+@register.simple_tag()
+def instructors_count():
+    instructors = Users.objects.filter(groups__name='Eğitmen')
+    return instructors.count()
+
+
+@register.simple_tag()
+def videos_count():
+    videos = Videos.objects.all()
+    return videos.count()
