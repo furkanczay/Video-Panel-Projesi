563f65c0df064b7da1b508bfce3acf4f67b61019
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-26 23:55:32 +0300

    Eğitmen video yükleyince discorda bildirim gitmesi için webhook kütüphanesi yüklendi ve ayarları yapıldı

--- apps/core/signals.py
+++ apps/core/signals.py
@@ -1,6 +1,9 @@
-from django.db.models.signals import post_migrate
+from django.db.models.signals import post_migrate, post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import Group
+from apps.core.models import Videos
+from discord_webhook import DiscordWebhook
+from apps.core.templatetags.site_settings import get_general_settings
 
 
 @receiver(post_migrate)
@@ -10,3 +13,21 @@ def create_groups(sender, **kwargs):
         Group.objects.get_or_create(name='Eğitmen')
         Group.objects.get_or_create(name='Personel')
         Group.objects.get_or_create(name='Yönetici')
+
+
+@receiver(post_save, sender=Videos)  # VideoModel, video modelinizi temsil eden model adı ile değiştirin
+def send_discord_message(sender, instance, created, **kwargs):
+    webhook_url = "https://discord.com/api/webhooks/1117070557117165578" \
+                  "/Js0tR8skdwMxJi6_3Po_GVV0kg9ykySqWdlpqkJ7O2uioLjCStJXZ6jbQlxin1G1Gpnf"
+    instructor = instance.instructor.get_full_name()
+    video_title = instance.title
+    video = instance.video_file
+    general_settings = get_general_settings()
+    if created:
+        message = f"{instructor}, {video_title} isminde yeni bir video paylaştı\nizlemek için: {general_settings.site_url}{video.url}"
+        webhook = DiscordWebhook(url=webhook_url, content=message)
+        webhook.execute()
+    else:
+        message = f"**{video_title}**, isimli video **{instructor}** {general_settings.site_url}{video.url} tarafından düzenlendi"
+        webhook = DiscordWebhook(url=webhook_url, content=message)
+        webhook.execute()
