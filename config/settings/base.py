ef01760462f73e338d8f5dbde87e877e77315d3e
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-27 12:23:16 +0300

    Hassas bilgiler environ ile gizlendi

--- config/settings/base.py
+++ config/settings/base.py
@@ -141,10 +141,10 @@ MEDIA_URL = '/media/'
 
 # SMTP EMAIL OPTION
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'furkanczay@gmail.com'
-EMAIL_HOST_PASSWORD = 'csogyfrtpplygurl'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 # DEFAULT PRIMARY KEY OPTION
