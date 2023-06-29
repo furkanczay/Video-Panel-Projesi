b0dd76da36b212a9dc9be18c261a8b2fb05e54ea
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-29 11:40:11 +0300

    Ticket sayfaları için viewlar hazırlandı

--- apps/modules/ticket/views.py
+++ apps/modules/ticket/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def tickets(request):
+    return render(request, 'pages/tickets.html', {})
+
+
+def ticket(request, ticket_id):
+    return render(request, 'pages/ticket.html', {})
+
+
+def ticket_new(request):
+    return render(request, 'pages/ticket_new.html', {})
+
+
+def ticket_edit(request, ticket_id):
+    return render(request, 'pages/ticket_edit.html', {})
+
+
+def ticket_delete(request, ticket_id):
+    pass
+
+
+def ticket_comment(request, ticket_id):
+    pass
+
+
+def ticket_comment_edit(request, ticket_id, comment_id):
+    pass
+
+
+def ticket_comment_delete(request, ticket_id, comment_id):
+    pass
