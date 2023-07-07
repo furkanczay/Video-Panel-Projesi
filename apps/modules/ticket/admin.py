6c49e4150ab85c7aa43fe9e1be7bee12ed051aa0
Furkan Özay <furkann.ozay@hotmail.com>
2023-07-07 22:40:46 +0300

    Ticket modülü, ihtiyaç olmaması sebebiyle kaldırıldı

--- apps/modules/ticket/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-admin.site.register(Ticket)
-admin.site.register(TicketStatuses)
-admin.site.register(TicketPriorities)
-admin.site.register(TicketCategories)
-admin.site.register(TicketMessages)
--- apps/modules/ticket/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import tickets, ticket_detail, ticket_new
-
-urlpatterns = [
-    path('', tickets, name='tickets'),
-    path('<int:ticket_id>/', ticket_detail, name='ticket_detail'),
-    path('new/', ticket_new, name='ticket_new'),
-]
